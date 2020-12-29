8a5dbec9cb097fabf72edbde4b7c88c744dff7aa
jadedandroid <lacroixjaded@gmail.com>
2020-12-29 12:01:46 -0500

    admin setup

--- app/core/models.py
+++ app/core/models.py
@@ -6,11 +6,22 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         # Creates and saves a new user
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('Users must have a valid email')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+    def create_superuser(self, email, password)
+    # creates and saves a staff / superuser
+    user = self.create_user(email, password)
+    user.is_staff = True
+    user.is_superuser = True
+    user.save(using=self.db)
+
+    return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     # custom user model that supports email instead of username
